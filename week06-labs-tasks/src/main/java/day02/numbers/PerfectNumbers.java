3f20d5c61703c138fe3133f6dc31800dae44ed8e
AdamZsoldos <adam.zsoldos@reddwarfgames.com>
2021-11-16 15:43:58 +0100

    Added day02 classes

---
+++ week06-labs-tasks/src/main/java/day02/numbers/PerfectNumbers.java
@@ -0,0 +1,16 @@
+package day02.numbers;
+
+public class PerfectNumbers {
+
+    public boolean isPerfectNumber(int number) {
+        int sum = 1;
+
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+
+        return sum == number;
+    }
+}
